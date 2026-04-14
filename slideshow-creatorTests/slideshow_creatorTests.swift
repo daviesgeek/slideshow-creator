292247caf7d3defa6b68271135b8471ca2ccebb4
Matthew Davies <matthew@daviesgeek.com>
2026-04-14 12:56:44 -0700

    Initial Commit

---
+++ slideshow-creatorTests/slideshow_creatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  slideshow_creatorTests.swift
+//  slideshow-creatorTests
+//
+//  Created by Matthew Davies on 4/14/26.
+//
+
+import Testing
+@testable import slideshow_creator
+
+struct slideshow_creatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
